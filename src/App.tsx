0dc64d82369f9a186e0fef3a5c2983c6d98fb69b
Hariharan <hariharanweb@gmail.com>
2025-05-30 13:53:48 +0530

    Professional Summary

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
     <div className='p-8'>
       <div className='flex p-4 justify-between border-2 border-indigo-200'>
         <div>
-          <h1 className="text-3xl font-bold to-blue-800">
+          <h1 className="text-3xl font-bold">
             Hariharan Thiagarajan
           </h1>
           <h3>
@@ -35,6 +35,17 @@ const App = () => {
             <a href='https://www.linkedin.com/in/hariharant/' className='pl-2'>linkedin.com/in/hariharant/</a>
           </div>
         </div>
+      </div>
+      <div className='p-4'>
+        <h2 className="text-xl pb-2">
+          PROFESSIONAL SUMMARY
+        </h2>
+        <ul className="list-disc pl-8">
+          <li>Tech leader with 17+ years of experience in designing and developing large scale enterprise applications.</li>
+          <li>Full stack developer with experience in web, backend, mobile development and devops.</li>
+          <li>Experience in multiple domains like travel, e-commerce and BFSI.</li>
+          <li>Leading teams in distributed delivery setups.</li>
+        </ul>
       </div>
     </div>
   );
